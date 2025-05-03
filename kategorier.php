c3d3445b7ea531f21fb485a3993b5fbaf56ece42
nadja <nadja.merrild.k@hotmail.com>
2025-05-03 19:12:25 +0200

    indsat kategorier og styling. skal gennemgåes, da css ikke ser ud til at virke ordentligt.

---
+++ kategorier.php
@@ -0,0 +1,103 @@
+<<?php
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>ErhvervsAward</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<?php
+include ("includes/navbar.php");
+?>
+
+<!--Kategoriboks-->
+<aside class="container categories">
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <h3>Kategorier</h3>
+            <a href="#erhvervsprisen">Erhvervsprisen</a>
+            <a href="#frontløberprisen">Frontløberprisen</a>
+            <a href="#initiavtivprisen">Initiativprisen</a>
+            <a href="#iværksætterprisen">Iværksætterprisen</a>
+            <a href="#klimaogmiljøprisen">Klima- og miljøprisen</a>
+            <a href="#praktikprisen">Praktikprisen</a>
+            <a href="#åretsleder">Årets leder</a>
+        </div>
+</aside>
+
+
+<!-- Main Content -->
+        <main class="content">
+            <article id="erhvervsprisen">
+                <h2>Erhvervsprisen</h2>
+                <p>
+                    ffff
+                </p>
+            </article>
+
+            <article id="frontløberprisen">
+                <h2>Frontløberprisen</h2>
+                <p>
+                    ffff
+                </p>
+            </article>
+
+            <article id="initiavtivprisen">
+                <h2>Initiavprisen</h2>
+                <p>
+                    ffff
+                </p>
+            </article>
+
+            <article id="iværksætterprisen">
+                <h2>Iværksætterprisen</h2>
+                <p>
+                    ffff
+                </p>
+            </article>
+
+            <article id="klimaogmiljøprisen">
+                <h2>Klima- og miljøprisen</h2>
+                <p>
+                    ffff
+                </p>
+            </article>
+
+            <article id="praktikprisen">
+                <h2>Praktikprisen</h2>
+                <p>
+                    ffff
+                </p>
+            </article>
+
+            <article id="åretsleder">
+                <h2>Årets leder</h2>
+                <p>
+                    ffff
+                </p>
+            </article>
+        </main>
+    </div>
+
+<?php
+include ("includes/footer.php");
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
