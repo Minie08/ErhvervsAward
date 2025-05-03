b1d4a0ca36ceb4acba5a03802ed1061f55f595c4
Betul <betula@live.dk>
2025-05-03 15:51:11 +0200

    footer og arkiv oprettet

---
+++ includes/footer.php
@@ -0,0 +1,54 @@
+<footer class="bg-dark text-white py-5 mt-5">
+    <div class="container">
+        <div class="row">
+
+            <!-- Tilmelding -->
+            <div class="col-md-4 mb-4">
+                <form class="d-flex">
+                    <input type="email" class="form-control rounded-start-pill me-2" placeholder="Indsæt din e-mail" aria-label="E-mail">
+                    <button class="btn btn-warning rounded-end-pill" type="submit">Tilmeld</button>
+                </form>
+                <div class="mt-4">
+                    <p class="mb-0">Foreningen ErhvervsAward</p>
+                    <p class="mb-0">Sdr. Stationsvej 26</p>
+                    <p class="mb-0">4200 Slagelse</p>
+                </div>
+            </div>
+
+            <!-- Navigation kolonne -->
+            <div class="col-md-4 mb-4">
+                <ul class="list-unstyled">
+                    <li><a href="/om-os.php" class="text-white text-decoration-none">Om Erhvervs Awards</a></li>
+                    <li><a href="/kontakt-os.php" class="text-white text-decoration-none">Kontakt Os</a></li>
+                    <li><a href="/arkiv.php" class="text-white text-decoration-none">Arkiv</a></li>
+                </ul>
+            </div>
+
+            <!-- Navigation kolonne -->
+            <div class="col-md-4 mb-4">
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white text-decoration-none">Awards vindere</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Bliv sponsor</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Sponsorer</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- Links og sociale medier -->
+        <div class="row mt-4">
+            <div class="col-md-8">
+                <ul class="list-inline">
+                    <li class="list-inline-item"><a href="#" class="text-white text-decoration-none">Privatlivspolitik</a></li>
+                    <li class="list-inline-item"><a href="#" class="text-white text-decoration-none">Handelsbetingelser</a></li>
+                    <li class="list-inline-item"><a href="#" class="text-white text-decoration-none">Cookiepolitik</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 text-md-end">
+                <a href="#"><i class="bi bi-facebook text-white me-3 fs-4"></i></a>
+                <a href="#"><i class="bi bi-instagram text-white me-3 fs-4"></i></a>
+                <a href="#"><i class="bi bi-youtube text-white me-3 fs-4"></i></a>
+                <a href="#"><i class="bi bi-linkedin text-white fs-4"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
