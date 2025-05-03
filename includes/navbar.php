44eefb6c0e6b509dd65f085c703501151eb2b6b8
Betul <betula@live.dk>
2025-05-03 12:50:04 +0200

    små justeringer

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary" id="navbar">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/index.php"><h1><strong>ErhvervsAward</strong></h1></a>
+        <a class="navbar-brand" href="/forside.php"><h1><strong>ErhvervsAward</strong></h1></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -9,27 +9,27 @@
 
                 <!--Priser-->
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/Priser.php">Priser</a>
+                    <a class="nav-link active" aria-current="page" href="/priser.php">Priser</a>
                 </li>
 
                 <!--Sponsorerne-->
                 <li class="nav-item">
-                    <a class="nav-link" href="/Sponsorerne.php">Sponsorerne</a>
+                    <a class="nav-link" href="/sponsorerne.php">Sponsorerne</a>
                 </li>
 
                 <!--Nominering-->
                 <li class="nav-item">
-                    <a class="nav-link" href="/Nominering.php">Nominering</a>
+                    <a class="nav-link" href="/nominering.php">Nominering</a>
                 </li>
 
                 <!--Om os-->
                 <li class="nav-item">
-                    <a class="nav-link" href="/Om-os.php">Om os</a>
+                    <a class="nav-link" href="/om-os.php">Om os</a>
                 </li>
 
                 <!--Kontakt os-->
                 <li class="nav-item">
-                    <a class="nav-link" href="/Kontakt-os.php">Kontakt os</a>
+                    <a class="nav-link" href="/kontakt-os.php">Kontakt os</a>
                 </li>
 
                 <!--Søgefelt-->
