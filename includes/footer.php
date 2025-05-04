4dfca4e43c0dec84cf7ea025a968fcca620e7eae
Betul <betula@live.dk>
2025-05-04 13:08:05 +0200

    footer opdatering, nye sider

--- includes/footer.php
+++ includes/footer.php
@@ -1,44 +1,42 @@
 <footer class="bg-dark text-white py-5">
         <div class="footer-container">
-                <div class="row">
-                    <div class="col-6 col-md-2 mb-3">
+                <div class="row justify-content-between">
+                    <!--Nyhedsmail-->
+                    <div class="col-12 col-md-4 mb-3 ps-4">
+                        <form>
+                            <h5 class="fw-bold">Nyhedsmail</h5>
+                            <p>Tilmeld dig vores nyhedsmail og modtag seneste nyt og opdateringer.</p>
+                            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                                <label for="newsletter" class="visually-hidden"></label>
+                                <input id="newsletter" type="email" class="form-control w-50 me-2" placeholder="">
+                                <button class="btn btn-warning w-25" type="button">Tilmeld</button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <!--Navigation-->
+                    <div class="col-12 col-md-5 mb-3 text-end pe-4">
                         <ul class="nav flex-column">
-                            <li class="nav-item mb-2"><a href="#" class="p-0 text-white">Om ErhvervsAward</a></li>
-                            <li class="nav-item mb-2"><a href="#" class="p-0 text-white">Awards vindere</a></li>
-                            <li class="nav-item mb-2"><a href="#" class="p-0 text-white">Bliv sponsor</a></li>
+                            <li class="nav-item mb-2"><a href="/om-os.php" class="p-0 text-white">Om ErhvervsAward</a></li>
+                            <li class="nav-item mb-2"><a href="/prismodtagere.php" class="p-0 text-white">Prismodtagere</a></li>
+                            <li class="nav-item mb-2"><a href="/bliv-sponsor.php" class="p-0 text-white">Bliv sponsor</a></li>
                             <li class="nav-item mb-2"><a href="/kontakt-os.php" class="p-0 text-white">Kontakt os</a></li>
                             <li class="nav-item mb-2"><a href="/arkiv.php" class="p-0 text-white">Arkiv</a></li>
                         </ul>
                     </div>
-
-                    <div class="col-6 col-md-2 mb-3">
-                        <ul class="nav flex-column">
-                            <li class="nav-item mb-2"><a href="#" class="p-0 text-white"></a></li>
-                            <li class="nav-item mb-2"><a href="#" class="p-0 text-white"></a></li>
-                            <li class="nav-item mb-2"><a href="#" class="p-0 text-white"></a></li>
-                            <li class="nav-item mb-2"><a href="#" class="p-0 text-white"></a></li>
-                            <li class="nav-item mb-2"><a href="#" class="p-0 text-white"></a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-6 col-md-2 mb-3">
-                    </div>
-
-                    <div class="col-md-5 offset-md-1 mb-3">
-                        <form>
-                            <h5 class="fw-bold">Nyhedsmail</h5>
-                            <p>Tilmeld dig vores nyhedsmail og modtag seneste nyt og opdateringer fra os i din e-mail</p>
-                            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
-                                <label for="newsletter" class="visually-hidden"></label>
-                                <input id="newsletter" type="email" class="form-control w-50 me-2" placeholder="">
-                                <button class="btn btn-warning" type="button">Tilmeld</button>
-                            </div>
-                        </form>
-                    </div>
                 </div>
 
+            <!--Oplysninger-->
+            <div class="text-center mt-4">
+            <ul class="list-unstyled mb-0">
+                <li class="mb-0">Foreningen ErhvervsAward</li>
+                <li class="mb-0">Sdr. Stationsvej 26</li>
+                <li class="mb-0">4200 Slagelse</li>
+            </ul>
+            </div>
+
             <!--Links-->
-            <div class="row justify-content-center">
+            <div class="row justify-content-center mt-5">
                 <div class="col-md-8 d-flex justify-content-center">
                     <ul class="list-inline mb-0">
                         <li class="list-inline-item"><a href="#" class="text-white text-decoration-none">Privatlivspolitik</a></li>
@@ -46,51 +44,5 @@
                         <li class="list-inline-item"><a href="#" class="text-white text-decoration-none">Cookiepolitik</a></li>
                     </ul>
                 </div>
-            </footer>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <!--<div class="row">
-
-            <div class="row">
-                <div class="col-md-5">
-                    <p class="mb-0">Foreningen ErhvervsAward</p>
-                    <p class="mb-0">Sdr. Stationsvej 26</p>
-                    <p class="mb-0">4200 Slagelse</p>
-                </div>
-
-            <!-- Navigation kolonne1 -->
-            <div class="col-md-4 mb-4">
-                <ul class="list-unstyled">
-                    <li><a href="/om-os.php" class="text-white text-decoration-none">Om Erhvervs Awards</a></li>
-                    <li><a href="/kontakt-os.php" class="text-white text-decoration-none">Kontakt Os</a></li>
-                    <li><a href="/arkiv.php" class="text-white text-decoration-none">Arkiv</a></li>
-                </ul>
             </div>
-
-            <!-- Navigation kolonne2 -->
-            <div class="col-md-4 mb-4">
-                <ul class="list-unstyled">
-                    <li><a href="#" class="text-white text-decoration-none">Awards vindere</a></li>
-                    <li><a href="#" class="text-white text-decoration-none">Bliv sponsor</a></li>
-                    <li><a href="#" class="text-white text-decoration-none">Sponsorer</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
 </footer>
