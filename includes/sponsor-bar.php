aabfb8dbf65a33168e5c994c0854d3fd49509575
Betul <betula@live.dk>
2025-05-05 12:05:46 +0200

    Tilføjelse af sponsor pic og små justeringer på sponsor-bar

--- includes/sponsor-bar.php
+++ includes/sponsor-bar.php
@@ -1,7 +1,7 @@
 <section class="sponsor-bar py-3 bg-light border-top">
     <div class="container overflow-hidden">
         <div class="d-flex flex-nowrap justify-content-around align-items-center gap-4 sponsor-track">
-            <img src="sponsor1.png" alt="Sponsor 1" height="40">
+            <img src="" alt="Sponsor 1" height="40">
             <img src="sponsor2.png" alt="Sponsor 2" height="40">
             <img src="sponsor3.png" alt="Sponsor 3" height="40">
             <img src="sponsor4.png" alt="Sponsor 4" height="40">
