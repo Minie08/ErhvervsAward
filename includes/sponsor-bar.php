86bb188d10bf00dcac685f9dae98f3ac638c6869
Betul <betula@live.dk>
2025-05-05 22:44:28 +0200

    ændringer

--- includes/sponsor-bar.php
+++ includes/sponsor-bar.php
@@ -1,11 +1,20 @@
-<section class="sponsor-bar py-3 bg-light border-top">
+<section class="sponsor-bar py-3 bg-light border-top" aria-label="Sponsorer">
     <div class="container overflow-hidden">
-        <div class="d-flex flex-nowrap justify-content-around align-items-center gap-4 sponsor-track">
-            <img src="" alt="Sponsor 1" height="40">
-            <img src="sponsor2.png" alt="Sponsor 2" height="40">
-            <img src="sponsor3.png" alt="Sponsor 3" height="40">
-            <img src="sponsor4.png" alt="Sponsor 4" height="40">
-            <!-- Tilføj flere logoer efter behov -->
+        <div class="sponsor-marquee">
+            <ul class="sponsor-track list-unstyled m-0">
+                <li><img src="/pic/Beierholm Logo.png" alt="Beierholm logo" height="40"></li>
+                <li><img src="/pic/BusinessSlagelse Logo.png" alt="Business Slagelse logo" height="40"></li>
+                <li><img src="/pic/Envafors Logo.png" alt="Envafors logo" height="40"></li>
+                <li><img src="/pic/KorsørErhvervsforening Logo.png" alt="Korsør Erhvervsforening logo" height="40"></li>
+                <li><img src="/pic/Skel Logo.png" alt="Skel logo" height="40"></li>
+                <li><img src="/pic/SkælskørErhvervs Logo.png" alt="Skælskør Erhvervsforening logo" height="40"></li>
+                <li><img src="/pic/Slagelseråd Logo.png" alt="Slagelse Råd logo" height="40"></li>
+                <li><img src="/pic/Sydbank Logo.png" alt="Sydbank logo" height="40"></li>
+                <li><img src="/pic/Zealand Logo.png" alt="Zealand logo" height="40"></li>
+                <!-- Dupliker for scrolling -->
+                <li><img src="/pic/Beierholm Logo.png" alt="Beierholm logo" height="40"></li>
+                <li><img src="/pic/BusinessSlagelse Logo.png" alt="Business Slagelse logo" height="40"></li>
+            </ul>
         </div>
     </div>
 </section>
