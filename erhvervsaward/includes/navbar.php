7b15f4e6ba2afae1517c237dad41211a847d97b5
nadja <nadja.merrild.k@hotmail.com>
2025-05-06 11:41:11 +0200

    init2

---
+++ erhvervsaward/includes/navbar.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary" id="navbar">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/index.php">
+            <header class="h4 m-0"><strong>ErhvervsAward</strong></header>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse text-end pe-5" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-lg-center gap-2">
+
+                <!-- Sponsorerne -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/sponsorerne.php">Sponsorerne</a>
+                </li>
+
+                <!-- Nominering -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/nominering.php">Nominering</a>
+                </li>
+
+                <!-- Priser -->
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="/priser.php">Priser</a>
+                </li>
+
+                <!-- Om os -->
+                <li class="nav-item">
+                    <a class="nav-link text-nowrap" href="/om-os.php">Om os</a>
+                </li>
+
+                <!-- Kontakt os -->
+                <li class="nav-item">
+                    <a class="nav-link text-nowrap" href="/kontakt-os.php">Kontakt os</a>
+                </li>
+
+                <!-- Søgefelt-->
+                <li class="nav-item w-100 w-lg-auto d-flex justify-content-end">
+                    <form class="d-flex gap-2" role="search">
+                        <input class="form-control form-control-sm w-745 w-lg-50" type="search" placeholder="Søg..." aria-label="Search">
+                        <button class="btn btn-sm btn-outline-warning" type="submit">Søg</button>
+                    </form>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav
