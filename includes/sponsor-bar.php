9d0c05ac3f0d6b5c10aef40551b1ff4dcd8bc16d
Betul <betula@live.dk>
2025-05-06 09:06:35 +0200

    sponsor-bar snart færdig

--- includes/sponsor-bar.php
+++ includes/sponsor-bar.php
@@ -2,16 +2,15 @@
     <div class="container-fluid overflow-hidden">
         <div class="sponsor-marquee">
             <ul class="sponsor-track d-flex flex-nowrap align-items-center gap-4 list-unstyled m-0">
-                <li><img src="/pic/Beierholm Logo.png" alt="Beierholm" height="40"></li>
-                <li><img src="/pic/BusinessSlagelse Logo.png" alt="Business Slagelse" height="40"></li>
-                <li><img src="/pic/Envafors Logo.png" alt="Envafors" height="40"></li>
-                <li><img src="/pic/ErhvervsAwards Sort Logo.png" alt="ErhvervsAwards" height="40"></li>
-                <li><img src="/pic/KorsørErhvervsforening Logo.png" alt="Korsør Erhvervsforening" height="40"></li>
-                <li><img src="/pic/Skel Logo.png" alt="Skel" height="40"></li>
-                <li><img src="/pic/SkælskørErhvervs Logo.png" alt="Skælskør Erhvervsforening" height="40"></li>
-                <li><img src="/pic/Slagelseråd Logo.png" alt="Slagelse Råd" height="40"></li>
-                <li><img src="/pic/Sydbank Logo.png" alt="Sydbank" height="40"></li>
-                <li><img src="/pic/Zealand Logo.png" alt="Zealand" height="40"></li>
+                <li><div class="logo-wrapper"><img src="/pic/Beierholm Logo.png" alt="Beierholm"></div></li>
+                <li><div class="logo-wrapper"><img src="/pic/BusinessSlagelse Logo.png" alt="Business Slagelse"></div></li>
+                <li><div class="logo-wrapper"><img src="/pic/Envafors Logo.png" alt="Envafors" height="40"></div></li>
+                <li><div class="logo-wrapper"><img src="/pic/KorsørErhvervsforening Logo.png" alt="Korsør Erhvervsforening"></div></li>
+                <li><div class="logo-wrapper"><img src="/pic/Skel Logo.png" alt="Skel"></div></li>
+                <li><div class="logo-wrapper"><img src="/pic/SkælskørErhvervs Logo.png" alt="Skælskør Erhvervsforening"></div></li>
+                <li><div class="logo-wrapper"><img src="/pic/Slagelseråd Logo.png" alt="Slagelse Råd"></div></li>
+                <li><div class="logo-wrapper"><img src="/pic/Sydbank Logo.png" alt="Sydbank"></div></li>
+                <li><div class="logo-wrapper"><img src="/pic/Zealand Logo.png" alt="Zealand"></div></li>
             </ul>
         </div>
     </div>
