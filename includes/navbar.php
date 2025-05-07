c61fc8a599b6a26f33c391cc4fd8e5a72e8dedd4
Betul <betula@live.dk>
2025-05-07 19:02:43 +0200

    mørk navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary" id="navbar">
+<nav class="navbar navbar-expand-lg bg-dark" id="navbar">
     <div class="container-fluid">
         <a class="navbar-brand" href="/index.php">
-            <header class="h4 m-0"><strong>ErhvervsAward</strong></header>
+            <header class="nav-header h4 m-0"><strong>ErhvervsAward</strong></header>
         </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -10,7 +10,7 @@
         </button>
 
         <div class="collapse navbar-collapse text-end pe-5" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-lg-center gap-2">
+            <ul class="navbar-nav text-light ms-auto mb-2 mb-lg-0 align-items-lg-center gap-2">
 
                 <!-- Sponsorerne -->
                 <li class="nav-item">
@@ -41,7 +41,7 @@
                 <li class="nav-item w-100 w-lg-auto d-flex justify-content-end">
                     <form class="d-flex gap-2" role="search">
                         <input class="form-control form-control-sm w-745 w-lg-50" type="search" placeholder="Søg..." aria-label="Search">
-                        <button class="btn btn-sm btn-outline-warning" type="submit">Søg</button>
+                        <button class="btn btn-sm btn btn-cta" type="submit">Søg</button>
                     </form>
                 </li>
 
